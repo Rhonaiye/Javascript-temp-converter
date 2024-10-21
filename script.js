408a7579c0965125cba1059af8b4bd8402a71817
Rhonaiye <itsrodivine@gmail.com>
2024-10-21 11:40:53 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const display = document.getElementById('display')
+const firstSelect = document.getElementById('slct1')
+const secondSelect = document.getElementById('slct2')
+const input = document.getElementById('input')
+const btn = document.getElementById('Changebtn')
+const answerTxt = document.getElementById('answer-text')
 
 
-function appendtoDisplay(input){
-    display.value += input
-}
+btn.onclick = function(){
+    
+    if(firstSelect.value == 'Fahrenheit' && secondSelect.value == 'Celsius'){
+        console.log('its is Fahrenheit and Celsius')
 
+        const Fahinput = input.value
+        let answer = (Fahinput - 32) * 5/9
+        let limitedDec = answer.toFixed(2)
+        
+        answerTxt.textContent = `°${limitedDec}C`
+    }
+    
 
-function clearDisplay(){
-    display.value = ''
-}
+    if(firstSelect.value == 'Celsius' && secondSelect.value == 'Fahrenheit'){
 
-function Calculate(){
-    let answer = eval(display.value)
-    display.value = answer
+        const Celinput = input.value
+        let answer = (9/5 * Celinput ) + 32
+        let limitedDec = answer.toFixed(2)
+        
+        answerTxt.textContent = `°${limitedDec}F`
+    }
+
+    if(input.value == ''){
+        answerTxt.textContent = 'Input a number'
+    }
+
+    if(firstSelect.value == secondSelect.value){
+        answerTxt.textContent = `Please select different units ${firstSelect.value} to ${secondSelect.value} does not require a conversion`
+    }
+
 }
 
 
